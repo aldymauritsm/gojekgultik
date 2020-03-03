1cddd356e0db948e5b06435e152106eae8f092f4
aldymauritsm <61737603+aldymauritsm@users.noreply.github.com>
2020-03-03 21:13:04 +0700

    crot.php

--- crot.php
+++ crot.php
@@ -133,7 +133,7 @@ function claim($token)
 	}
 
 echo "AUTO REGISTER, KLAIM VOUCHER, GOPAY SENDER V3\n";
-print "Thanks to : Muhammad Ikhsan, Muhammad Jumadi, Muhammad Irsad\n\n";
+print "Thanks to : Aldi dorCROT, EPENDORCROT\n\n";
 echo "[+] Masukan 62 untuk ID dan 1 untuk US\n";
 echo "[+] Nomor: ";
 $nope = trim(fgets(STDIN));
@@ -284,39 +284,4 @@ $oce = gopaysend($a, $no);
 echo "".$oce."\n";
            }
 	  	 }
-
-         setpin:
-         echo "\n".color("yellow","?] SET PIN DULU BIAR AMAN!!!: y/n ");
-         $pilih1 = trim(fgets(STDIN));
-         if($pilih1 == "y" || $pilih1 == "Y"){
-         //if($pilih1 == "y" && strpos($no, "628")){
-         echo color("white","========( PIN MU = 112233 )========")."\n";
-         $data2 = '{"pin":"112233"}';
-         $getotpsetpin = request("/wallet/pin", $token, $data2, null, null, $uuid);
-         echo "Otp pin: ";
-         $otpsetpin = trim(fgets(STDIN));
-         $verifotpsetpin = request("/wallet/pin", $token, $data2, null, $otpsetpin, $uuid);
-         echo $verifotpsetpin;
-         }else if($pilih1 == "n" || $pilih1 == "N"){
-         die();
-         }else{
-         echo color("red","-] GAGAL!!!\n");
-         }
-         }
-         goto setpin;
-         }
-         }else{
-            echo color("red","-] Otp yang anda input salah");
-            echo"\n==================================\n\n";
-            echo color("yellow","!] Silahkan input kembali\n");
-            goto otp;
-            }
-         }else{
-         echo color("red","-] Nomor sudah teregistrasi");
-         echo"\n==================================\n\n";
-         echo color("yellow","!] Silahkan registrasi kembali\n");
-         goto ulang;
-         }
-//  }
-
-// echo change()."\n";
+?>

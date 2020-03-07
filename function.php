2da83e1e9c934b0b455f8b3fc5eec45ffe92d972
aldymauritsm <61737603+aldymauritsm@users.noreply.github.com>
2020-03-08 01:03:54 +0700

    function.php

--- function.php
+++ function.php
@@ -1,39 +1,93 @@
-<?php  
-function curl($url, $data = null, $headers = null, $proxy = null) {
+<?php
 
-	$ch = curl_init();
-	$options = array(
-		CURLOPT_URL => $url,
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_SSL_VERIFYHOST => 0,
-		CURLOPT_SSL_VERIFYPEER => 0,
-		CURLOPT_HEADER => true,
-		CURLOPT_TIMEOUT => 30,
-                CURLOPT_FOLLOWLOCATION => true,
-	);
+function request($url, $token = null, $data = null, $pin = null, $otpsetpin = null, $uuid = null){
 
-	if ($data != "") {
-		$options[CURLOPT_POST] = true;
-		$options[CURLOPT_POSTFIELDS] = $data;
-	}
-
-	if ($proxy != "") {
-		$options[CURLOPT_HTTPPROXYTUNNEL] =  true;
-		$options[CURLOPT_PROXYTYPE] =  CURLPROXY_SOCKS4;
-		$options[CURLOPT_PROXY] =  $proxy;
-	}
-
-	if ($headers != "") {
-		$options[CURLOPT_HTTPHEADER] = $headers;
-	}
-
-	curl_setopt_array($ch, $options);
-	$result = curl_exec($ch);
-	curl_close($ch);
-	return $result;
-
+$header[] = "Host: api.gojekapi.com";
+$header[] = "User-Agent: okhttp/3.10.0";
+$header[] = "Accept: application/json";
+$header[] = "Accept-Language: id-ID";
+$header[] = "Content-Type: application/json; charset=UTF-8";
+$header[] = "X-AppVersion: 3.30.2";
+$header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
+$header[] = "Connection: keep-alive";
+$header[] = "X-User-Locale: id_ID";
+$header[] = "X-Location: -6.917464,107.619122";
+$header[] = "X-Location-Accuracy: 3.0";
+if ($pin):
+$header[] = "pin: $pin";
+    endif;
+if ($token):
+$header[] = "Authorization: Bearer $token";
+endif;
+if ($otpsetpin):
+$header[] = "otp: $otpsetpin";
+endif;
+if ($uuid):
+$header[] = "User-uuid: $uuid";
+endif;
+$c = curl_init("https://api.gojekapi.com".$url);
+    curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+    if ($data):
+    curl_setopt($c, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($c, CURLOPT_POST, true);
+    endif;
+    curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($c, CURLOPT_HEADER, true);
+    curl_setopt($c, CURLOPT_HTTPHEADER, $header);
+    $response = curl_exec($c);
+    $httpcode = curl_getinfo($c);
+    if (!$httpcode)
+        return false;
+    else {
+        $header = substr($response, 0, curl_getinfo($c, CURLINFO_HEADER_SIZE));
+        $body   = substr($response, curl_getinfo($c, CURLINFO_HEADER_SIZE));
+    }
+    $json = json_decode($body, true);
+    return $body;
 }
-
+function save($filename, $content)
+{
+    $save = fopen($filename, "a");
+    fputs($save, "$content\r\n");
+    fclose($save);
+}
+function nama()
+    {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://ninjaname.horseridersupply.com/dutch_name.php");
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $ex = curl_exec($ch);
+    // $rand = json_decode($rnd_get, true);
+    preg_match_all('~(&bull; (.*?)<br/>&bull; )~', $ex, $name);
+    return $name[2][mt_rand(0, 14) ];
+    }
+function getStr($a,$b,$c){
+	$a = @explode($a,$c)[1];
+	return @explode($b,$a)[0];
+}
+function getStr1($a,$b,$c,$d){
+        $a = @explode($a,$c)[$d];
+        return @explode($b,$a)[0];
+}
+function color($color = "default" , $text)
+    {
+        $arrayColor = array(
+            'grey'      => '1;30',
+            'red'       => '1;31',
+            'green'     => '1;32',
+            'yellow'    => '1;33',
+            'blue'      => '1;34',
+            'purple'    => '1;35',
+            'nevy'      => '1;36',
+            'white'     => '1;0',
+        );  
+        return "\033[".$arrayColor[$color]."m".$text."\033[0m";
+    }
 function fetch_value($str,$find_start,$find_end) {
 	$start = @strpos($str,$find_start);
 	if ($start === false) {
@@ -43,50 +97,4 @@ function fetch_value($str,$find_start,$find_end) {
 	$end    = strpos(substr($str,$start +$length),$find_end);
 	return trim(substr($str,$start +$length,$end));
 }
-
-function getcookies($source) {
-	preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $source, $matches);
-	$cookies = array();
-	foreach($matches[1] as $item) {
-		parse_str($item, $cookie);
-		$cookies = array_merge($cookies, $cookie);
-	}
-	return $cookies;
-}
-
-function number($length) {
-	$characters = '0123456789';
-	$charactersLength = strlen($characters);
-	$randomString = '';
-	for ($i = 0; $i < $length; $i++) {
-		$randomString .= $characters[rand(0, $charactersLength - 1)];
-	}
-	return $randomString;
-}
-
-function string($length) {
-	$characters = 'abcdefghijklmopqrstuvwxyz';
-	$charactersLength = strlen($characters);
-	$randomString = '';
-	for ($i = 0; $i < $length; $i++) {
-		$randomString .= $characters[rand(0, $charactersLength - 1)];
-	}
-	return $randomString;
-}
-
-function nama()
-	{
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, "http://ninjaname.horseridersupply.com/indonesian_name.php");
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	$ex = curl_exec($ch);
-	// $rand = json_decode($rnd_get, true);
-	preg_match_all('~(&bull; (.*?)<br/>&bull; )~', $ex, $name);
-	return $name[2][mt_rand(0, 14) ];
-	}
-
-
 ?>

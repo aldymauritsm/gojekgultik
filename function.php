686c3596cbeeefe279031412190b53b0f2f6dbc1
aldymauritsm <61737603+aldymauritsm@users.noreply.github.com>
2020-06-26 13:08:21 +0700

    function.php

--- function.php
+++ function.php
@@ -11,8 +11,8 @@ $header[] = "X-AppVersion: 3.30.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -5.147665,119.4327313";
-$header[] = "X-Location-Accuracy: 10.0";
+$header[] = "X-Location: -6.923201,107.618538";
+$header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
     endif;

5665858c558693d4b3bab29cdd052cfe70f29e5a
aldymauritsm <61737603+aldymauritsm@users.noreply.github.com>
2020-07-28 00:17:18 +0700

    refresh.php

--- refresh.php
+++ refresh.php
@@ -13,7 +13,7 @@ echo "\n".color("white","Claim?: y/n ");
         echo color("white",".");
         sleep(5);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOOD2107"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD2107"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("green"," Message: ".$message);

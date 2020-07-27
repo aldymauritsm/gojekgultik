e0efc87c7d227eae8283ad938e603722cf9818f2
aldymauritsm <61737603+aldymauritsm@users.noreply.github.com>
2020-07-27 23:51:59 +0700

    function.php

--- function.php
+++ function.php
@@ -3,15 +3,15 @@
 function request($url, $token = null, $data = null, $pin = null, $otpsetpin = null, $uuid = null){
 
 $header[] = "Host: api.gojekapi.com";
-$header[] = "User-Agent: okhttp/3.12.1";
+$header[] = "User-Agent: okhttp/3.10.0";
 $header[] = "Accept: application/json";
 $header[] = "Accept-Language: id-ID";
 $header[] = "Content-Type: application/json; charset=UTF-8";
-$header[] = "X-AppVersion: 3.53.1";
+$header[] = "X-AppVersion: 3.56.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -8.584825,116.101706";
+$header[] = "X-Location: -8.424012,115.194628";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
